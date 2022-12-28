0b4c0d5f9add9c8e698e198709c47743ac6a7ff6
Nick Art <nicoltri@student.ethz.ch>
2022-12-28 11:42:13 +0000

    fixed path length

---
+++ EyeTrackingIssue/Assets/DisplaySSCoordinates.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR;
+
+public class DisplaySSCoordinates : MonoBehaviour
+{
+    public Transform debugSphere;
+    public Text displayText;
+    public Camera vrCamera;
+    
+    private void Update()
+    {
+        var coords = vrCamera.WorldToScreenPoint(debugSphere.position, Camera.MonoOrStereoscopicEye.Left);
+        var eyeTextureSize = new Vector2(XRSettings.eyeTextureWidth, XRSettings.eyeTextureHeight);
+        var units = new Vector2(coords.x / eyeTextureSize.x, coords.y / eyeTextureSize.y);
+        displayText.text = $"({units.x:F2},{units.y:F2})";
+    }
+}
