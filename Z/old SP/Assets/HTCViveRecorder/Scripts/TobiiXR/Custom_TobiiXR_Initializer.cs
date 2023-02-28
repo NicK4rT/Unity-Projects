85cd53a9863acb5f2a156d6ef25673023da0799a
Nick Art <nicoltri@student.ethz.ch>
2023-02-28 13:08:44 +0100

    final submission

---
+++ Z/old SP/Assets/HTCViveRecorder/Scripts/TobiiXR/Custom_TobiiXR_Initializer.cs
@@ -0,0 +1,24 @@
+﻿using Tobii.XR;
+using UnityEngine;
+
+// Customized TobiiXR Initializer (HTC Vive Pro Eye)
+public class Custom_TobiiXR_Initializer : MonoBehaviour
+{
+
+    public TobiiXR_Settings Settings;
+
+    private void Awake()
+    {
+        TobiiXR.Start(Settings);
+        
+    }
+
+    public void OnDisable()
+    {
+        TobiiXR.Stop();
+        Destroy(this.gameObject);
+    }
+
+   
+
+}
